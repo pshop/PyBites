aa239144d3755d7bfc7f69fdacaea250484dc922
pshop <choppin.p@gmail.com>
2019-10-12 15:40:27 +0200

    cleaning code

--- bites/data_analysis_path/analyse_sales_data.py
+++ bites/data_analysis_path/analyse_sales_data.py
@@ -11,9 +11,7 @@ if not path.isfile(EXCEL):
 def load_excel_into_dataframe(excel=EXCEL):
     """Load the SalesOrders sheet of the excel book (EXCEL variable)
        into a Pandas DataFrame and return it to the caller"""
-    xls = pd.ExcelFile(excel)
-    df = pd.read_excel(xls, 'SalesOrders')
-    return df
+    return pd.read_excel(excel, 'SalesOrders')
 
 
 def get_year_region_breakdown(df):
@@ -21,9 +19,7 @@ def get_year_region_breakdown(df):
        column. You probably need to make an extra column for
        year, return the new df as shown in the Bite description"""
     df = df.rename(columns={'OrderDate': 'Year'})
-    df_total = df.groupby([df['Year'].dt.year, "Region"])['Total'].sum()
-    return df_total
-
+    return df.groupby([df['Year'].dt.year, "Region"])['Total'].sum()
 
 
 def get_best_sales_rep(df):
@@ -31,16 +27,17 @@ def get_best_sales_rep(df):
        the total of his/her sales"""
     df_sales = df.groupby('Rep').sum()
     id_max = df_sales['Total'].idxmax()
-    return (id_max, df_sales.loc[id_max]['Total'])
+    return id_max, df_sales.loc[id_max]['Total']
+
 
 def get_most_sold_item(df):
     """Return a tuple of the name of the most sold item
        and the number of units sold"""
     df_item =df.groupby('Item').sum()
     id_max = df_item['Units'].idxmax()
-    return (id_max, df_item.loc[id_max]['Units'])
+    return id_max, df_item.loc[id_max]['Units']
+
 
 if __name__ == '__main__':
     df = load_excel_into_dataframe()
-
     print(get_best_sales_rep(df))
